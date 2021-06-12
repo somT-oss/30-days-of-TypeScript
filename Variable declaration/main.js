2d6fb9707986c45b06b7f83bbbef60669651bf31
somT-oss <somtochukwuuchegbu@gmail.com>
2021-06-12 19:22:53 +0100

    Added package.json

--- Variable declaration/main.js
+++ Variable declaration/main.js
@@ -12,6 +12,7 @@ function doSomething() {
     for (var i = 1; i < 5; i++) {
         console.log(i);
     }
-    console.log("Finally " + i);
+    // console.log("Finally " + i);
 }
 doSomething();
+// For declaring of varibles using the let keyword, the varible cannot be accessible outside the block the varible the was decleared, hence the error. We still got our javascript file after compilling it that is becuase the error doesn't stop the compilling but shows us the error we have in order to fix it before compilling
--- Variable declaration/main.ts
+++ Variable declaration/main.ts
@@ -22,7 +22,7 @@ function doSomething() {
     for (let i = 1; i < 5; i++) {
         console.log(i);
     }
-    console.log("Finally " + i);
+    // console.log("Finally " + i);
 }
 
 doSomething();
