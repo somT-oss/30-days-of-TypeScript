bb38b301cb36949bdecf497211d4c5fcdfdc30ff
somT-oss <somtochukwuuchegbu@gmail.com>
2021-06-16 07:03:21 +0100

    Day 6, arrow functions. Skipped day 5

---
+++ arrow functions/main.js
@@ -0,0 +1,11 @@
+// Normal function 
+var log = function (message) {
+    console.log(message);
+};
+// arrow function 
+// This helps write cleaner code and help organise your workflow better, it's also an added advantage if you have knowledge of it.
+var doLog = function (message) {
+    console.log(message);
+};
+// Or write it more cleaner 
+var doLogTwo = function (message) { return console.log(message); };
---
+++ arrow functions/main.ts
@@ -0,0 +1,16 @@
+// Normal function 
+
+let log = function(message) {
+    console.log(message);
+}
+
+// arrow function 
+// This helps write cleaner code and help organise your workflow better, it's also an added advantage if you have knowledge of it.
+
+let doLog = (message) => {
+    console.log(message)
+}
+
+// Or write it more cleaner 
+
+let doLogTwo = (message) => console.log(message); 
